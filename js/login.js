307a0ab32b4383ae5d1e53f7ab580045b6ccda11
igorgaev2014 <59573818+igorgaev2014@users.noreply.github.com>
2020-06-24 13:35:42 +0500

    24.06 test 1

--- js/login.js
+++ js/login.js
@@ -107,7 +107,7 @@ function showSlides(n) {
     slideIndex = 1
   }
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
